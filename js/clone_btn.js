94e4fb2772763002cdafaafca1196eda74eed43d
hojunahn <hojunahn@naver.com>
2024-04-21 06:01:09 +0900

    끝

--- js/clone_btn.js
+++ js/clone_btn.js
@@ -1,8 +1,10 @@
-prevButton = document.getElementById('prevButton');
+
+const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
 const cell = document.getElementById('cell');
-let totalSlides = cell.children.length;
+const slides = cell.querySelectorAll('.txt_area');
 let currentSlide = 0;
+const slidesPerView = 4; // 화면에 보여지는 슬라이드 수
 
 prevButton.addEventListener('click', (event) => {
     event.preventDefault();
@@ -14,25 +16,24 @@ prevButton.addEventListener('click', (event) => {
 
 nextButton.addEventListener('click', (event) => {
     event.preventDefault();
-    if (currentSlide < totalSlides - 4) {
+    if (currentSlide < slides.length - slidesPerView) {
         currentSlide++;
         moveSlider();
     }
 });
 
-// 슬라이드 이동 함수
 function moveSlider() {
-    const slideWidth = calculateSlideWidth();
+    const slideWidth = slides[currentSlide].offsetWidth;
     const newPosition = -currentSlide * slideWidth;
     cell.style.transform = `translateX(${newPosition}px)`;
 
-  
+    // 현재 슬라이드가 처음이나 마지막 슬라이드인지 확인하여 버튼 활성/비활성화
+    prevButton.disabled = currentSlide === 0;
+    nextButton.disabled = currentSlide === slides.length - slidesPerView;
 }
 
-// 슬라이드 너비 계산
-function calculateSlideWidth() {
-    return cell.clientWidth / 4; // 4는 slidesPerView 값
-}
+// 창 크기 변경 시 슬라이드 이동 함수 다시 호출
+window.addEventListener('resize', moveSlider);
 
 // 초기화
 moveSlider();
